f610a4c0fd71f5e23b7bdb2b7d2bd22d9751182e
Nursultan Mukhametzhanov <mukhametzhanovnurs@gmail.com>
2023-10-27 10:02:58 +0600

    Добавление функции поиска даты в формате AM/PM

--- CsvAnalyst.py
+++ CsvAnalyst.py
@@ -1,11 +1,14 @@
 # Программа анализа .csv файлов
 
+from distutils.cygwinccompiler import check_config_h
+from re import T
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText as st
 from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 import os
 import pandas as pd
+from tomlkit import item
 
 #  Создание главного окна
 window = tk.Tk()
@@ -104,17 +107,87 @@ def list_meet_name(fields_list):
     return False, ratio
 
 
+# Если в этом поле дата в формате AM/PM, пусть вернет True
+def meet_date_am_pm(field):
+    checkfor = [" AM", " PM"]
+    for s in checkfor:
+        if s in str(field):  # Нашлось!
+            return True
+    # Ничего не совпало
+    return False
+
+
+# Если в этом списке многие элементы содержат дату в формате AM/PM, пусть вернет True
+def list_meet_data_am_pm(fields_list):
+    counter_total = 0
+    counter_meet = 0
+    for list_item in fields_list:
+        counter_total += 1
+        if meet_date_am_pm(list_item):
+            counter_meet += 1
+    # Конец подсчёта
+    ratio = counter_meet / counter_total
+    if ratio > 0.5:
+        return True, ratio
+    # Не набралось нужного количество совпадений
+    return False, ratio
+
+
 # Пройти все столбцы
 def check_all_columns(df):
     columns_count = df.shape[1]
     for i in range(columns_count):  # от 0 до columns_count-1
         lst = get_column(df, i)
+        # Первый критерий
         result = list_meet_name(lst)
         if result[0]:
-            output_text.insert(tk.END, "В столбце " + str(i+1) + " предположительно содержится имя." + os.linesep)
-            output_text.insert(tk.END, "Процент совпадений " + "{:.2f}".format(result[1] * 100) + "%." + os.linesep)
-        else:
-            output_text.insert(tk.END, "Предположений для столбца " + str(i+1) + " не найдено." + os.linesep)
+            output_text.insert(
+                tk.END,
+                "В столбце "
+                + str(i + 1)
+                + " предположительно содержится имя."
+                + os.linesep,
+            )
+            output_text.insert(
+                tk.END,
+                "Процент совпадений "
+                + "{:.2f}".format(result[1] * 100)
+                + "%."
+                + os.linesep
+                + os.linesep,
+            )
+            continue  # Все нашли, можно идти к следующему столбцу
+
+        # Второй критерий
+        result_2 = list_meet_data_am_pm(lst)
+        if result_2[0]:
+            output_text.insert(
+                tk.END,
+                "В столбце "
+                + str(i + 1)
+                + " предположительно содержится дата в формате AM/PM."
+                + os.linesep,
+            )
+            output_text.insert(
+                tk.END,
+                "Процент совпадений "
+                + "{:.2f}".format(result_2[1] * 100)
+                + "%."
+                + os.linesep
+                + os.linesep,
+            )
+            continue  # Все нашли, можно идти к следующему столбцу
+
+        # Cоответствия критериям не найдено
+        output_text.insert(
+            tk.END,
+            "Предположений для столбца "
+            + str(i + 1)
+            + " не найдено."
+            + os.linesep
+            + os.linesep,
+        )
+
 
 # Обработчик нажатия кнопки
 def process_btn():
